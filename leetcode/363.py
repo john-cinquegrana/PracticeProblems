f3ea536eaf00a9434c9e71d0d857aeafbb7a6d30
John Cinquegrana <john.cinquegrana@rutgers.edu>
2024-03-26 15:21:33 +0000

    Problem to finish

---
+++ leetcode/363.py
@@ -0,0 +1,19 @@
+'''
+File:			363.py
+Project:		Practice Problems
+File Created:	Thursday, January 1st 1970
+Remote:			PracticeProblems at 'https://github.com/john-cinquegrana/PracticeProblems'
+Author(s):		John Cinquegrana (alllegron@gmail.com)
+
+Copyright 2024 John Cinquegrana (alllegron@gmail.com)
+
+https://leetcode.com/problems/max-sum-of-rectangle-no-larger-than-k/
+'''
+
+__author__		=	"John Cinquegrana"
+__copyright__	=	"Copyright 2024 John Cinquegrana (alllegron@gmail.com)"
+
+class Solution:
+    def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
+        # TODO
+        pass
