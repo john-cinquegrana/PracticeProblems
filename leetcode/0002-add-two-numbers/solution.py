fdc583b1965cf779f90a534d2d781acfebce6d3b
Jazz <alllegron@gmail.com>
2024-06-24 15:54:55 +0000

    [LeetCode Sync] Runtime - 60 ms (27.63%), Memory - 16.7 MB (30.16%)

--- leetcode/0002-add-two-numbers/solution.py
+++ leetcode/0002-add-two-numbers/solution.py
@@ -1,62 +1,26 @@
 # Definition for singly-linked list.
-
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-        
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        def carry_copy(ls, carry):
-            ''' Copy a list while considering a carry from previous addition. '''
-            if ls == None:
-                if carry == 0:
-                    return None
-                else: return ListNode(val=carry)
-            
-            # Perform the arithmetic
-            sum = ls.val + carry
-            new_carry = 0 if (sum < 10) else 1
-            ones_col = sum if (sum < 10) else (sum-10)
-            
-            #Calculate the later nodes
-            next_node = carry_copy(ls.next, new_carry)
-            
-            return ListNode(val=ones_col, next=next_node)
-        
-        def add(first, second, carry, acc):
-            ''' Recursively add together the two lists, including a carry, into the
-            accumulator variable. The accumulator variable points to the end of
-            the linked list containing the sum'''
-            # Check to see if one list is empty
-            if first == None:
-                # First list is empty
-                # carry_copy takes care of the case when they're both empty
-                acc.next = carry_copy( second, carry )
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def tail_recurse(l1, l2, result, carry):
+            # Base Case
+            if l1 is None and l2 is None and carry==0:
                 return
-            if second == None:
-                acc.next = carry_copy( first, carry )
-                return
-            # If one list is empty, the sum is simply the other list and the carry
-            
-            # We are sure both lists have at least one number in them
-            # We perform the arithmetic
-            sum = first.val + second.val + carry
-            new_carry = 0 if (sum < 10) else 1
-            ones_col = sum if (sum < 10) else (sum-10)
-            
-            # Place the next node and iterate
-            step = ListNode(val=ones_col)
-            acc.next = step
-            add(first.next, second.next, new_carry, step)
+            # Set up the values for addition
+            l1_value = 0 if l1 is None else l1.val
+            l2_value = 0 if l2 is None else l2.val
+            # Set up the pointers for the next run
+            l1_next = None if l1 is None else l1.next
+            l2_next = None if l2 is None else l2.next
+            # Perform the addition
+            value = l1_value + l2_value + carry
+            new_carry = 1 if value >= 10 else 0
+            result.next = ListNode(value % 10)
+            return tail_recurse(l1_next, l2_next, result.next, new_carry)
+        ll_head = ListNode()
+        tail_recurse(l1, l2, ll_head, 0)
+        return ll_head.next
         
-        # Create a dummy head to the sum list
-        head = ListNode()
-        add(l1, l2, 0, head)
-        return head.next
-            
